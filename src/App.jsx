d0a680a171c3bcfe85b5260446a74ce4a162092a
DylanNavarro97 <dylanrubennavarro@hotmail.com>
2023-12-27 01:51:37 -0300

    maquetado de componente frase, y lo inclui en App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,20 @@
-function App() {
+import { Button } from "react-bootstrap";
+import "./App.css";
+import Frase from "./components/Frase";
+import SimpsonsLogo from "./image/logoSimpson.png";
 
+function App() {
   return (
-    <>
-      
-    </>
-  )
+    <section>
+      <div className="d-flex justify-content-center">
+        <img src={SimpsonsLogo} alt="Simpson Logo" className="logoImg" />
+      </div>
+      <Frase></Frase>
+      <div className="d-flex justify-content-center">
+        <Button variant="warning" className="my-4">Obtener frase</Button>
+      </div>
+    </section>
+  );
 }
 
-export default App
+export default App;
--- src/components/Frase.jsx
+++ src/components/Frase.jsx
@@ -1,9 +1,22 @@
+import { Container } from "react-bootstrap";
+import MoeImage from "../image/moe.png";
+
 export default function Frase() {
   return (
-    <section>
-        <h2>Moe Szyslak</h2>
-        <hr></hr>
-        
-    </section>
-  )
+    <Container className="border border-2 border-danger p-5">
+      <h2 className="text-center">Moe Szyslak</h2>
+      <hr></hr>
+      <div className="d-flex justify-content-center">
+        <img src={MoeImage} alt="Moe Szyslak"/>
+      </div>
+      <div className="bg-light p-4 text-center">
+        <p className="fs-4">
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Soluta, reprehenderit beatae obcaecati animi minima explicabo asperiores quas eligendi vero cum deserunt quae izpsam inventore repellendus amet itaque, libero fuga enim.
+        </p>
+        <p className="fs-5">
+            Moe Szyslak in The Simpson
+        </p>    
+      </div>
+    </Container>
+  );
 }
